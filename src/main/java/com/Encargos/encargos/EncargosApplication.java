3b0fa9df5beff60d258662c8f58c4fa6395d5c57
Lorena Daniela Fiordelmondo <lfiordemondo@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-06-24 11:33:07 -0300

    inicio

---
+++ src/main/java/com/Encargos/encargos/EncargosApplication.java
@@ -0,0 +1,13 @@
+package com.Encargos.encargos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncargosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncargosApplication.class, args);
+	}
+
+}
